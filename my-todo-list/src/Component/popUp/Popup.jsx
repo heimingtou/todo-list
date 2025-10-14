c600f2b7b9451ac83a574b23096184d765e4aba8
heimingtou <haminhtho175@gmail.com>
2025-10-14 21:54:55 +0700

    dung useReducer thay cho useState

--- my-todo-list/src/Component/popUp/Popup.jsx
+++ my-todo-list/src/Component/popUp/Popup.jsx
@@ -1,26 +1,36 @@
 
-import { useState } from "react";
+import { useReducer, useState } from "react";
 import Input from "../Input/Input";
-import Title from "../Title/Title";
 import "./Popup.css"
 export default function Popup({onAdd,onClose}){
-    const [list,setList]=useState([""])
+    const [list,dispatch]=useReducer(reducer,[""])
     const[title,setTitle]=useState("")
     // them input list
-    function add(){
+//     function add(){
         
-        setList([...list,""])
-    }
-    // xoa input list
-   function del(index){
-        setList(list.filter((_,i)=>i!==index))
+//         setList([...list,""])
+//     }
+//     // xoa input list
+//    function del(index){
+//         setList(list.filter((_,i)=>i!==index))
+//    }
+   function reducer(list,action){
+        switch (action.type){
+            case 'add':
+                return [...list,""];
+            case 'delete':
+                return list.filter((_,i)=>i!==action.id)
+            case 'change':
+                return list.map((item,i)=>i==action.id?action.value:item)
+
+        }
    }
    // cap nhat lai mang list 
-   function handleChange(e, index){
-        let newList=[...list];
-        newList[index]=e.target.value;
-        setList(newList)
-   }
+//    function handleChange(e, index){
+//         let newList=[...list];
+//         newList[index]=e.target.value;
+//         setList(newList)
+//    }
    function handleAddTask(){
     if(title===""){
         onClose()
@@ -36,14 +46,14 @@ export default function Popup({onAdd,onClose}){
    }
    let renderList = list.map((_, i) => (
   <div key={i} style={{ display: "flex" }}>
-    <button className="btn" onClick={() => del(i)}>-</button>
-    <Input placeholder={`List ${i + 1}`} className="list" value={list[i]} onChange={(e)=>handleChange(e,i)} />
+    <button className="btn" onClick={() => dispatch({type:'delete',id:i})}>-</button>
+    <Input placeholder={`List ${i + 1}`} className="list" value={list[i]} onChange={(e)=>dispatch({type:'change',id: i,value:e.target.value})} />
   </div>
 ));
     return(
         <div className="Popup">
             <Input placeholder="Title" className="titles" value={title} onChange={(e)=>setTitle(e.target.value)} />
-            <button className="btn" onClick={add} >+</button>
+            <button className="btn" onClick={()=>dispatch({type:'add'})} >+</button>
             {renderList}
             <button className="btn-save" onClick={handleAddTask}>Save</button>
         </div>
